a9885b43db92ea9f2ff5b26babff55ebc9cdcbcd
Adriano Gil <adrianogil.san@gmail.com>
2019-07-27 08:15:02 -0400

    Added function to generate header files

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -10,3 +10,20 @@ function devcpp-cmake-build()
     build_folder=build
     cmake --build ${build_folder} -- -j3
 }
+
+function devcpp-gen-header-class()
+{
+    class_name=$1
+
+    header_filename=$(echo ${class_name//_} | tr '[:upper:]' '[:lower:]')'.h'
+    header_macro=$(echo ${class_name//_} | tr '[:lower:]' '[:upper:]')'_H'
+
+    echo "#ifndef "${header_macro} >  ${header_filename}
+    echo "#define "${header_macro} >> ${header_filename}
+    echo "  "                      >> ${header_filename}
+    echo "class "${class_name}   >> ${header_filename}
+    echo "{ "                      >> ${header_filename}
+    echo "}; "                     >> ${header_filename}
+    echo "  "                      >> ${header_filename}
+    echo "#endif "                 >> ${header_filename}
+}
